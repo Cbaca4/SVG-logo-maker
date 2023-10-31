13b1f321d7aabdc619d8bb1d8ba84b761070ba81
Cristian <139021004+Cbaca4@users.noreply.github.com>
2023-10-31 01:27:31 -0500

    image not popping up

--- Lib/shape.test.js
+++ Lib/shape.test.js
@@ -6,7 +6,7 @@ describe("Triangle test", () => {
 const shape = new Triangle();
 shape.setColor("blue");
 expect(shape.render()).toEqual(
-    '<polygon points="150, 18 244, 182 56, 182" fill=${this.color}" />'
+    '<polygon points="150, 18 244, 182 56, 182" fill="blue" />'
 );
 });
 });
@@ -16,7 +16,7 @@ describe("Square test", () => {
 const shape = new Square();
 shape.setColor("purple");
 expect(shape.render()).toEqual(
-    '<rect x="73", y="40" width="160" height="160" fill=${this.color}" />'
+    '<rect x="73" y="40" width="160" height="160" fill="purple" />'
 );
 });
 });
@@ -27,7 +27,7 @@ describe("Circle test", () => {
 const shape = new Circle();
 shape.setColor("#ca00ca");
 expect(shape.render()).toEqual(
-    '<circle cx="150", cy="115" r="80" fill=${this.color}" />'
+    '<circle cx="150" cy="115" r="80" fill="#ca00ca" />'
 );
 });
 });
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const {Triangle, Square, Circle} = require("./lib/shapes");
+const {Triangle, Square, Circle} = require("./Lib/shape");
 function writeToFile(fileName, answers) {
     let svgString = "";
     svgString = '<svg version="1.1" width="300" height="200">';
@@ -15,13 +15,13 @@ if (answers.shape === "triangle") {
     shapeChoice = new Square();
     svgString += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeBackgroundColor}"/>`;
    } else {
-    shapeChoice = new circle();
+    shapeChoice = new Circle();
     svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackgroundColor}"/>`;
    }
    
    svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textcolor}">${answers.text}</text>`;
-   svgString  += "</g";
-   svgString += "</svg";
+   svgString  += "</g>";
+   svgString += "</svg>";
 
    fs.writeFile(fileName, svgString, (err) =>{
     err ? console.log(err) : console.log("generated logo.svg");
